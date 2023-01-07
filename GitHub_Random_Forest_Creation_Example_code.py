1f59fe5d91543d19e6cb0d475b1eba5165bbbbd5
MicroBatVet <109685166+MicroBatVet@users.noreply.github.com>
2023-01-07 15:49:19 -0400

    Update GitHub_Random_Forest_Creation_Example_code.py

--- GitHub_Random_Forest_Creation_Example_code.py
+++ GitHub_Random_Forest_Creation_Example_code.py
@@ -249,33 +249,3 @@ Tree_2 = tree.export_graphviz(forest_base.estimators_[2], out_file =None, filled
 graph = graphviz.Source(Tree_2, format='png')
 graph
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import seaborn as sb
-import matplotlib.pyplot as plt
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.model_selection import KFold
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import GridSearchCV
-
-
